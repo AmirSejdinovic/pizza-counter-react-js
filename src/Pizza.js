bf9d11ff51b12d318077a8dc5aff6ab217760623
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-19 10:38:25 +0200

    Creating Class Component to use State

---
+++ src/Pizza.js
@@ -0,0 +1,17 @@
+//Importing React and Component. This is because I want to build Class component. Class component is need because I want to use State. React State we can not use on Function Component
+import React,{Component}from 'react';
+
+//Creating the Class Pizza wich extends React.Component functions. This I use because I want to use State menagment
+class Pizza extends React.Component{
+  //Inside Class in React we need to have render method which returns HTML
+  render(){
+    return<div>
+
+    </div>
+  }
+
+}
+
+
+//Exporting the component function
+export default Pizza;
