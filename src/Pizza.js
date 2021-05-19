fd4a4ee0996d90581d17239d5d68393f9a1ce084
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-19 11:16:50 +0200

    Adding state to app

--- src/Pizza.js
+++ src/Pizza.js
@@ -18,17 +18,38 @@ const btn = {
 
 //Creating the Class Pizza wich extends React.Component functions. This I use because I want to use State menagment
 class Pizza extends React.Component{
+//To use the state it must have constructor in which will be pased props
+  constructor(props){
+       super(props)
+       //In the state i declar that count on start will be zero
+       this.state={
+          conut:0
+       }
+  }
+  //Creating methods for taking order
+  takeOrder(){
+   this.setState({
+    conut: this.state.conut + 1
+   })
+  }
+
+  //Creting method for cancle order
+  cancelOrder(){
+    this.setState({
+      conut: this.state.conut - 1
+    })
+  }
   //Inside Class in React we need to have render method which returns HTML
   render(){
     return<div>
      
     <img src={pizza} width="200"/>
     <h1>Pizza Order Counter</h1>
-    <h1 style={Counter}>0</h1>
-    <button style={btn}>Take order</button>
+    <h1 style={Counter}>{this.state.conut}</h1>
+    <button style={btn} onClick={()=>this.takeOrder()}>Take order</button>
     <br/>
     <br/>
-    <button style={btn}>Cancel order</button>
+    <button style={btn} onClick={()=>this.cancelOrder()}>Cancel order</button>
     </div>
   }
 
